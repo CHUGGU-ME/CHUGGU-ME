403508a50b7db25887fc65219366be6c74b19be3
나평근 <ko.itbuddy>
2023-11-10 01:06:37 +0900

    기반 코드 작성

--- src/main/kotlin/com/chuggume/chuggume/ChugguMeApplication.kt
+++ src/main/kotlin/com/chuggume/chuggume/ChugguMeApplication.kt
@@ -1,5 +1,6 @@
 package com.chuggume.chuggume
 
+import com.chuggume.chuggume.service.MomService
 import com.chuggume.chuggume.service.PlayerService
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
@@ -10,7 +11,8 @@ inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
 
 @SpringBootApplication
 class ChugguMeApplication(
-    private val playerService: PlayerService
+    private val playerService: PlayerService,
+    private val momService: MomService
 ) : CommandLineRunner{
     private val log = logger()
 
@@ -23,6 +25,7 @@ class ChugguMeApplication(
     fun menuGateWay(args: Array<out String?> ){
         when(args[0]){
             "player" -> playerService.service()
+            "mom" -> momService.service()
         }
     }
 }
