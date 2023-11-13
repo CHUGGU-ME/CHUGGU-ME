60c2e386ee0a4ef7b50ddedd15f1694484604b48
나평근 <ko.itbuddy>
2023-11-14 02:29:21 +0900

    Browser 실행 객체, 명령어 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ extra["springCloudVersion"] = "2021.0.8"
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    implementation("org.apache.commons:commons-lang3:3.13.0")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("com.microsoft.playwright:playwright:1.28.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
